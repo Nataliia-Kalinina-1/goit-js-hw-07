1955ed1630e6b2dda24fa1cf69731c3b368953ea
Nataliia Kalinina <kalininanatasha@gmail.com>
2025-10-27 06:21:19 +0100

    task 3&4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,36 @@
+const form = document.querySelector('.login-form');
+const { email, password } = form.elements;
 
+email.addEventListener('focus', () => {
+  email.placeholder = 'Type area';
+});
+password.addEventListener('focus', () => {
+  password.placeholder = 'Type area';
+});
+
+email.addEventListener('blur', () => {
+  email.placeholder = '';
+});
+password.addEventListener('blur', () => {
+  password.placeholder = '';
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+  form.reset();
+});
